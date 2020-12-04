2ddf1e54640e8989f2974023cb18fa7863c97c59
yukikimuramura <54853881+yukikimuramura@users.noreply.github.com>
2020-12-05 00:47:15 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
-// /*
-// * Copyright(C) 2020 YukiKimura
-//
+/*
+ * Copyright(C) 2020 Yuki Kimura+Ryuichi Ueda.All right reserved.
+*/
 
 
 #include <linux/module.h>
